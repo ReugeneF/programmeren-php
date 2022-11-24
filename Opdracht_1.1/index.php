fe2fd77c6f1791fe78b494dd824181ff77728bba
ReugeneF <reugene_business@hotmail.com>
2022-11-24 13:55:31 +0100

    o

--- Opdracht_1.1/index.php
+++ Opdracht_1.1/index.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hallow</title>
+</head>
+<body>
+    
+</body>
+</html>
 <?php
     echo "<h1> First <h1>";
     echo "Test <br>";
@@ -7,5 +19,10 @@
     $name = "Reugene Forst";
     echo "$name";
     ?>
-
+<body>
+    <?php
+    /* begroetingsregel tonen */
+        echo "hallo wereld";
+        ?>
+</body>
     
