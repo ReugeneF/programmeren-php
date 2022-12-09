8387c1791e576830d26b585e93f1cb8810c45fea
ReugeneF <reugene_business@hotmail.com>
2022-12-09 13:48:39 +0100

    yeah

---
+++ Opracht_3.2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $var1 = "Reugene";
+        $var2 = "Forst";
+        $n1 = 3;
+        $n2 = 6;
+       
+
+        echo "$var1 $var2 <br> $n1 $n2" ; 
+        ?>
+</body>
+</html>
